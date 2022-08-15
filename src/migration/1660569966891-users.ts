2688709ef62e11aceeb3ccf09a35f04e4e5e8944
Leonardorossato <leonardorossato@gmail.com>
2022-08-15 10:26:31 -0300

    migration do usuario criada

---
+++ src/migration/1660569966891-users.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class users1660569966891 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
