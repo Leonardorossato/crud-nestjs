5af29f2f9801789aacc9d6cdbb409de21c8654b0
Leonardorossato <leonardorossato@gmail.com>
2022-08-15 10:23:06 -0300

    limpeza

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
-import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
+import { User } from './users/entities/user.entity';
+import { Module } from '@nestjs/common';
 
 @Module({
-  imports: [UsersModule],
-  controllers: [],
-  providers: [],
+  imports: [UsersModule, TypeOrmModule.forFeature([User])]
 })
 export class AppModule {}
